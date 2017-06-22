f68136c77b9dfad886f2479f1a2ae475692667db
Nate Jones <nate@mediatemple.net>
2017-06-22 09:54:14 -0700

    adding cli flags

--- common/queue/recv/main.go
+++ common/queue/recv/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/justone/pulse_box/common/queue"
@@ -8,11 +9,18 @@ import (
 )
 
 func main() {
-	logrus.SetLevel(logrus.DebugLevel)
+	queueUrl := flag.String("queue", "", "SQS queue url")
+	debug := flag.Bool("debug", false, "show debug output")
+	flag.Parse()
+
+	logrus.SetLevel(logrus.InfoLevel)
+	if *debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
 	logrus.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
 
 	q, err := queue.NewSQS(queue.SQSConfig{
-		QueueUrl: "https://sqs.us-west-2.amazonaws.com/149259370426/pulse",
+		QueueUrl: *queueUrl,
 	})
 	if err != nil {
 		fmt.Println(err)
