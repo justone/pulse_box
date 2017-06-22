3377c0fc865fa7d873acf0218b43d16d3e37699d
Joshua Barratt <josh@mediatemple.net>
2017-06-22 11:36:32 -0700

    Basic twitter streaming reader

---
+++ emitters/twitter/main.go
@@ -0,0 +1,61 @@
+// heavily inspired by https://github.com/campoy/justforfunc/blob/master/14-twitterbot/main.go
+package main
+
+import (
+	"net/url"
+	"os"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	consumerKey       = getenv("TWITTER_CONSUMER_KEY")
+	consumerSecret    = getenv("TWITTER_CONSUMER_SECRET")
+	accessToken       = getenv("TWITTER_ACCESS_TOKEN")
+	accessTokenSecret = getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+func getenv(name string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		panic("missing required environment variable " + name)
+	}
+	return v
+}
+
+func main() {
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+
+	log := &logger{logrus.New()}
+	api.SetLogger(log)
+
+	stream := api.PublicStreamFilter(url.Values{
+		"track": []string{"@mediatemple", "#mediatemple", "media temple", "Media Temple", "hackathon"},
+		// @mediatemple
+		"follow": []string{"684983"},
+	})
+
+	defer stream.Stop()
+
+	for v := range stream.C {
+		t, ok := v.(anaconda.Tweet)
+		if !ok {
+			log.Warningf("received unexpected value of type %T", v)
+			continue
+		}
+
+		log.Infof("Tweet: %s", t.Text)
+	}
+}
+
+type logger struct {
+	*logrus.Logger
+}
+
+func (log *logger) Critical(args ...interface{})                 { log.Error(args...) }
+func (log *logger) Criticalf(format string, args ...interface{}) { log.Errorf(format, args...) }
+func (log *logger) Notice(args ...interface{})                   { log.Info(args...) }
+func (log *logger) Noticef(format string, args ...interface{})   { log.Infof(format, args...) }
