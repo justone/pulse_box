6e2290e2ea4a94379c88cbb070776bfb26fcbd7d
Nate Jones <nate@endot.org>
2017-07-20 07:50:40 -0700

    move to library, add command

---
+++ colortest/cmd/colortest/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/justone/pulse_box/colortest"
+)
+
+func main() {
+	height := flag.Int("height", 10, "height of the LED grid")
+	width := flag.Int("width", 10, "width of the LED grid")
+	logFile := flag.String("logfile", "hub.log", "filename to log messages to")
+
+	rand.Seed(time.Now().Unix())
+	flag.Parse()
+
+	f, err := os.OpenFile(*logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
+	if err := run(*height, *width); err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+}
+
+func run(height, width int) error {
+	driver, err := anim.NewScreenDriver(height, width)
+	if err != nil {
+		return err
+	}
+
+	animation, err := anim.NewRandomSinglePixel()
+	if err != nil {
+		return err
+	}
+
+	go driver.Render(animation)
+
+	<-driver.DoneChan()
+
+	return nil
+}
--- colortest/main.go
+++ colortest/main.go
@@ -1,11 +1,8 @@
-package main
+package anim
 
 import (
-	"flag"
-	"fmt"
 	"log"
 	"math/rand"
-	"os"
 	"time"
 
 	"github.com/gdamore/tcell"
@@ -28,50 +25,6 @@ type Animation interface {
 type Driver interface {
 	Render(Animation)
 	DoneChan() chan bool
-}
-
-func main() {
-	height := flag.Int("height", 10, "height of the LED grid")
-	width := flag.Int("width", 10, "width of the LED grid")
-	logFile := flag.String("logfile", "hub.log", "filename to log messages to")
-
-	rand.Seed(time.Now().Unix())
-	flag.Parse()
-
-	f, err := os.OpenFile(*logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
-	}
-	defer f.Close()
-
-	log.SetOutput(f)
-
-	if err := run(*height, *width); err != nil {
-		fmt.Println(err)
-		os.Exit(2)
-	}
-}
-
-func run(height, width int) error {
-	var driver Driver
-	var animation Animation
-
-	driver, err := NewScreenDriver(height, width)
-	if err != nil {
-		return err
-	}
-
-	animation, err = NewRandomSinglePixel()
-	if err != nil {
-		return err
-	}
-
-	go driver.Render(animation)
-
-	<-driver.DoneChan()
-
-	return nil
 }
 
 type RandomSinglePixel struct {
